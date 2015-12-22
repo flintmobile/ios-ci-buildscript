7db4c4b274e25844fe7d9233aa819a319ddd9572
PC Nguyen <pc@flint.com>
2015-12-22 13:39:10 -0800

    do not increment build number if we don't have changes

--- auto-increment-build.sh
+++ auto-increment-build.sh
@@ -4,26 +4,42 @@
 #
 
 PLIST_FILE="${XCS_SOURCE_DIR}/FlintCreditCard/FlintCardScanner/FlintCardScanner-Info.plist"
-LAST_BUILD_FILE="/Users/Shared/XcodeServer/lastBuildNum.log"
+LAST_BUILD_FILE="/Users/Shared/XcodeServer/lastBuildInfo.log"
 
-# Getting the build numbers as string
-PLIST_BUILD_STRING=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$PLIST_FILE")
-LAST_BUILD_STRING=$(<$LAST_BUILD_FILE)
+# Getting the plist build number
+PLIST_BUILD_NUM_STR=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$PLIST_FILE")
+PLIST_BUILD_NUM=$(($PLIST_BUILD_STRING+0))
+echo "current plist build: $PLIST_BUILD_NUM"
 
-# Casting them to number and increment
-PLIST_BUILD_NUM=$(($PLIST_BUILD_STRING+1))
-LAST_BUILD_NUM=$(($LAST_BUILD_STRING+1))
+# Getting the server last saved automatic build number
+LAST_BUILD_INFO_STRING=$(<$LAST_BUILD_FILE)
+LAST_BUILD_INFO=($LAST_BUILD_INFO_STRING)
+LAST_BUILD_NUM_STR=${LAST_BUILD_INFO[0]}
+LAST_BUILD_NUM=$(($LAST_BUILD_STRING+0))
+echo "current ci server build: $LAST_BUILD_NUM"
 
-echo "compare plist build $PLIST_BUILD_NUM and server build $LAST_BUILD_NUM"
 BUILD_NUM=$LAST_BUILD_NUM
 if [ $PLIST_BUILD_NUM -gt $LAST_BUILD_NUM ]; then
-echo "using plist build number"
-BUILD_NUM=$PLIST_BUILD_NUM
+	BUILD_NUM=$PLIST_BUILD_NUM
+fi
+echo "Using build number: $BUILD_NUM"
+
+# Comparing commmit hash to see if we actually have changes
+LAST_BUILD_HASH=${LAST_BUILD_INFO[1]}
+echo "Last build hash: $LAST_BUILD_HASH"
+
+GIT_SOURCE="${XCS_SOURCE_DIR}/FlintCreditCard"
+LATEST_COMMIT_HASH=$(git -C $GIT_SOURCE rev-parse HEAD)
+echo "Latest commit hash: $LASTEST_COMMIT_HASH"
+
+if [ $LAST_BUILD_HASH == $LATEST_COMMIT_HASH ]; then
+	echo "No new commit, not increment build"
+else
+	BUILD_NUM=$(($BUILD_NUM+1))
+	echo "Increase to build $BUILD_NUM"
+	/usr/libexec/Plistbuddy -c "Set CFBundleVersion $BUILD_NUM" "$PLIST_FILE"
 fi
 
-echo "increase to build $BUILD_NUM"
-/usr/libexec/Plistbuddy -c "Set CFBundleVersion $BUILD_NUM" "$PLIST_FILE"
-
-echo "update last build file"
-echo $BUILD_NUM > $LAST_BUILD_FILE
+echo "Clean up - Update last build info"
+echo "$BUILD_NUM $LATEST_COMMIT_HASH"> $LAST_BUILD_FILE
 cat $LAST_BUILD_FILE
